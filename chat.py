4d806bf1af402a7d4336d0096448e0a848de356e
Nick Pape <5674316+nick-pape@users.noreply.github.com>
2024-06-04 09:56:01 -0500

    Fix some indentation

--- chat.py
+++ chat.py
@@ -28,7 +28,7 @@ tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL_NAME, trust_remote_code=Tru
 tokenizer.pad_token = tokenizer.eos_token
 tokenizer.padding_side = "right"
 
-## The chat history
+## Chat history helper class.
 class ChatHistory:
     def __init__(self):
         self.__messages = []
@@ -50,34 +50,34 @@ class ChatHistory:
 history = ChatHistory()
 
 while True:
-        history.add_user_message(input("You: "))
-        prompt = history.tokenize(tokenizer)
+    history.add_user_message(input("You: "))
+    prompt = history.tokenize(tokenizer)
 
-        raw_output = model.generate(
-            **prompt,
-            ## limit the response length, else it could just go on and on and on and on and on and on and.....
-            max_length=SEQUENCE_LENGTH, 
-            ## we only need one, but you can do more
-            num_return_sequences=1, 
-            do_sample=True,
-            ## this is "beam search", which basically is a forking factor during generation. You can ramp this up, but it adds computation.
-            num_beams=4, 
-            ## this is sort of a hack, but basically I'm telling it that when we hit the first <end_of_turn> to treat it like an EOS.
-            eos_token_id=107,
-            ## tells the beams to stop searching if they hit EOS
-            early_stopping=True, 
-            ## you can read about these two... but basically they control the number of "possible sequences" during generation.
-            top_k=50, 
-            top_p=0.95,
-            ## randomness factor
-            temperature=0.7
-        )
+    raw_output = model.generate(
+        **prompt,
+        ## limit the response length, else it could just go on and on and on and on and on and on and.....
+        max_length=SEQUENCE_LENGTH, 
+        ## we only need one, but you can do more
+        num_return_sequences=1, 
+        do_sample=True,
+        ## this is "beam search", which basically is a forking factor during generation. You can ramp this up, but it adds computation.
+        num_beams=4, 
+        ## this is sort of a hack, but basically I'm telling it that when we hit the first <end_of_turn> to treat it like an EOS.
+        eos_token_id=107,
+        ## tells the beams to stop searching if they hit EOS
+        early_stopping=True, 
+        ## you can read about these two... but basically they control the number of "possible sequences" during generation.
+        top_k=50, 
+        top_p=0.95,
+        ## randomness factor
+        temperature=0.7
+    )
 
-        ## this line will certainly make Pete tear his eyes out.
-        ## but we are taking the first result (since num_return_sequences=1), and, since this includes the prompt,
-        ## we basically lop the prompt off the front. We are relying on `eos_token_id` above to hope these are short.
-        input_token_count = prompt['input_ids'].shape[1]
-        output = tokenizer.decode(raw_output[0][input_token_count:], skip_special_tokens=True)
+    ## this line will certainly make Pete tear his eyes out.
+    ## but we are taking the first result (since num_return_sequences=1), and, since this includes the prompt,
+    ## we basically lop the prompt off the front. We are relying on `eos_token_id` above to hope these are short.
+    input_token_count = prompt['input_ids'].shape[1]
+    output = tokenizer.decode(raw_output[0][input_token_count:], skip_special_tokens=True)
 
-        print("Model:", output)
-        history.add_bot_message(output)
+    print("Model:", output)
+    history.add_bot_message(output)
